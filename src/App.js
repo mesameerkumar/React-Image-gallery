72af292e529b3aa499486d75532eba0b9f257454
Sameer Kumar <mesameerkumar@gmail.com>
2021-07-15 19:44:47 +0530

    added card

--- src/App.js
+++ src/App.js
@@ -3,7 +3,25 @@ import React from "react";
 function App() {
   return (
     <div >
-      
+      <div className="max-w-sm rounded overflow-hidden shadow-2xl ">
+        <img src="https://source.unsplash.com/random" className="w-full"  alt="image"/>
+        <div className="px-6 py-4">
+          <div className="text-bold text-blue-500 text-xl mb-2">
+          Photo by xD
+          </div>
+          <ul>
+            <li>
+              <strong>Views: </strong>4000
+            </li>
+            <li>
+              <strong>Downloads: </strong>500
+            </li>
+            <li>
+              <strong>Likes: </strong>500
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
